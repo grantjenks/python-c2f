6279ae68fccdd8cb82f4177b86477ab4431793b8
Grant Jenks <grant.jenks@gmail.com>
2019-12-04 17:48:37 -0800

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     name='c2f',
-    version='1.0.1',
+    version='1.0.2',
     py_modules=['c2f'],
     ext_modules=cythonize('c2f.py'),
     long_description=open('README.rst').read(),
